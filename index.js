ed8406a1840a1386110343a80e8d0e6edd04e297
lek <abungan082198@gmail.com>
2025-05-31 16:56:52 +0800

    updated

--- index.js
+++ index.js
@@ -2,6 +2,10 @@
 
 const links = document.querySelectorAll("a");
 const navBar = document.querySelector("ul");
+const overlay = document.querySelector(".overlay");
+const overlay2 = document.querySelector(".overlay2");
+const details = document.querySelector(".card-details");
+const imgDetail = document.querySelector("#imgDetail");
 
 links.forEach((item) => {
   item.addEventListener("click", () => {
@@ -16,8 +20,31 @@ links.forEach((item) => {
 
 function openNav() {
   navBar.classList.add("show");
+  overlay.classList.add("show");
 }
 
 function closeNav() {
   navBar.classList.remove("show");
+  overlay.classList.remove("show");
 }
+
+function closeOverLay() {
+  closeNav();
+}
+
+function showDetails(img) {
+  imgDetail.src = img;
+  details.classList.add("details");
+  overlay2.classList.add("details");
+}
+
+function closeDetails() {
+  details.classList.remove("details");
+  overlay2.classList.remove("details");
+}
+
+function closeOverLay2() {
+  closeDetails();
+}
+
+console.log(overlay2);
