a12338daa9cbac0dace8014c22a4833230f93c1c
lek <abungan082198@gmail.com>
2025-06-20 20:24:27 +0800

    updated

--- index.js
+++ index.js
@@ -46,17 +46,6 @@ const challengeLinks = {
   },
 };
 
-links.forEach((item) => {
-  item.addEventListener("click", () => {
-    links.forEach((item) => {
-      item.classList.remove("active");
-    });
-
-    item.classList.add("active");
-    closeNav();
-  });
-});
-
 function openNav() {
   navBar.classList.add("show");
   overlay.classList.add("show");
@@ -95,4 +84,13 @@ function closeOverLay2() {
   closeDetails();
 }
 
-console.log(overlay2);
+links.forEach((item) => {
+  item.addEventListener("click", () => {
+    links.forEach((item) => {
+      item.classList.remove("active");
+    });
+
+    item.classList.add("active");
+    closeNav();
+  });
+});
